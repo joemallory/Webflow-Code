b173e65d0c1900a065fb6e14b4b501df8b2fd2da
Joe Mallory <joe@joe.digital>
2018-08-08 09:17:08 +0100

    Minified Jquery

--- window-resize.js
+++ window-resize.js
@@ -1,11 +1,2 @@
-$( window ).resize (function() {
- if ($(window).width() > 991) {
-   $('.nav__menu-inner').css('display', 'flex');
-   $('#nav-close-btn').css('display', 'none');
- }
-else {
- $('.nav__menu-inner').css('display', 'none');
- $('#nav-close-btn').css('display', 'none');
-
-}
-});
+$(window).resize(function(){if($(window).width()>991){$('.nav__menu-inner').css('display','flex');$('#nav-close-btn').css('display','none')}
+else{$('.nav__menu-inner').css('display','none');$('#nav-close-btn').css('display','none')}})
