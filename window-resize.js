4cde8c18414103ee793d8b42842e7de27a5a10f4
Joe Mallory <joe@joe.digital>
2018-08-09 11:43:07 +0100

    Added Loading Bar JS

--- window-resize.js
+++
@@ -1,2 +0,0 @@
-$(window).resize(function(){if($(window).width()>991){$('.nav__menu-inner').css('display','flex');$('#nav-close-btn').css('display','none')}
-else{$('.nav__menu-inner').css('display','none');$('#nav-close-btn').css('display','none')}})
